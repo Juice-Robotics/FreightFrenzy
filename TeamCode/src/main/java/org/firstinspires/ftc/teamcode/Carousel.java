8c97e2ee41d3b9da7dda49ebe8a2fb36a1518887
Rvenkat08 <93448427+Rvenkat08@users.noreply.github.com>
2022-01-27 18:21:59 -0800

    Add files via upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Carousel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Carousel.java
@@ -1,21 +1,47 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Carousel {
+import com.qualcomm.robotcore.util.Range;
+
+public class carousel {
+
     public Motor carousel;
+    double targetRPM = 30;
+
     public Carousel(Component carouselMotor){
-        this.carousel = (Motor) carouselMotor;
+        
+        this.carousel = (Motor) carouselMotor; 
     }
+    
+    final double KP = 0.0001;
+    final double KI = 0;
+    final double KD = 0;
+    
+    public double currentRPM = 0;
+    
+    boolean spinmotor = false;
+
+    PIDController spinmotorPID = new spinmotor (targetRPM, KP, KI, KD, false);
 
     public void start(){
 
+        spinmotor = true;
+    }
+   
 
+    public void updateRPM(){
+
+        if (targetRPM != currentRPM) {
+            spinmotorPID = new PIDController(targetRPM, KP, KI, KD, false);
+        }
+    
+        double correction = spinmotorPID.update(currentRPM);
+        
     }
 
     public void stop(){
-
-
-    }
-    public void reverse(){
-
+        spinmotor = false;
     }
 }
+
+
+
